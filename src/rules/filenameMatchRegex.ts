d672a3fe96ccee6a38bc03e06014008610001b0d
Gajus Kuizinas <gajus@gajus.com>
2023-01-07 14:25:35 -0600

    fix: move regex to upper scope

--- src/rules/filenameMatchRegex.ts
+++ src/rules/filenameMatchRegex.ts
@@ -12,20 +12,24 @@ import {
   parseFilename,
 } from '../utilities';
 
+// eslint-disable-next-line unicorn/no-unsafe-regex
+const defaultRegexp = /^[\da-z]+(?:[A-Z][\da-z]+)*$/ug;
+
 export default createRule({
   create: (context) => {
-    // eslint-disable-next-line unicorn/no-unsafe-regex
-    const defaultRegexp = /^[\da-z]+(?:[A-Z][\da-z]+)*$/ug;
     const conventionRegexp = context.options[0] ? new RegExp(context.options[0], 'u') : defaultRegexp;
     const ignoreExporting = context.options[1] ? context.options[1] : false;
 
     return {
       Program (node) {
+        conventionRegexp.lastIndex = 0;
+
         const filename = context.getFilename();
         const absoluteFilename = path.resolve(filename);
         const parsed = parseFilename(absoluteFilename);
         const shouldIgnore = isIgnoredFilename(filename);
         const isExporting = Boolean(getExportedName(node));
+
         const matchesRegex = conventionRegexp.test(parsed.name);
 
         if (shouldIgnore) {
