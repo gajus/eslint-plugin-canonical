5db39004d75093410fa296d66c3931428a913b5e
Gajus Kuizinas <gajus@gajus.com>
2023-01-09 11:10:22 -0600

    fix: rewrite id-match

--- src/rules/idMatch.ts
+++ src/rules/idMatch.ts
@@ -2,9 +2,8 @@
 /* eslint-disable complexity */
 /* eslint-disable func-style */
 
-import {
-  createRule,
-} from '../utilities';
+import { type TSESTree, AST_NODE_TYPES } from '@typescript-eslint/utils';
+import { createRule } from '../utilities';
 
 /**
  * @file Rule to flag non-matching identifiers
@@ -17,14 +16,11 @@ import {
 /**
  * Checks if a parent of a node is an ObjectPattern.
  *
- * @param {ASTNode} node The node to check.
  * @returns {boolean} if the node is inside an ObjectPattern
  * @private
  */
-const isInsideObjectPattern = (node) => {
-  let {
-    parent,
-  } = node;
+const isInsideObjectPattern = (node: TSESTree.Node) => {
+  let { parent } = node;
 
   while (parent) {
     if (parent.type === 'ObjectPattern') {
@@ -37,227 +33,254 @@ const isInsideObjectPattern = (node) => {
   return false;
 };
 
-const create = (context) => {
-  const pattern = context.options[0] || '^.+$';
-  const regexp = new RegExp(pattern, 'u');
-
-  const options = context.options[1] || {};
-  const checkProperties = Boolean(options.properties);
-  const checkClassFields = Boolean(options.classFields);
-  const onlyDeclarations = Boolean(options.onlyDeclarations);
-  const ignoreDestructuring = Boolean(options.ignoreDestructuring);
-  const ignoreNamedImports = Boolean(options.ignoreNamedImports);
-
-  // Contains reported nodes to avoid reporting twice on destructuring with shorthand notation
-  const reportedNodes = new Set();
-  const ALLOWED_PARENT_TYPES = new Set([
-    'CallExpression',
-    'NewExpression',
-  ]);
-  const DECLARATION_TYPES = new Set([
-    'FunctionDeclaration',
-    'VariableDeclarator',
-  ]);
-  const IMPORT_TYPES = new Set([
-    'ImportSpecifier',
-    'ImportNamespaceSpecifier',
-    'ImportDefaultSpecifier',
-  ]);
-
-  /**
-   * Checks if a string matches the provided pattern
-   *
-   * @param {string} name The string to check.
-   * @returns {boolean} if the string is a match
-   * @private
-   */
-  function isInvalid (name) {
-    return !regexp.test(name);
-  }
-
-  /**
-   * Verifies if we should report an error or not based on the effective
-   * parent node and the identifier name.
-   *
-   * @param {ASTNode} effectiveParent The effective parent node of the node to be reported
-   * @param {string} name The identifier name of the identifier node
-   * @returns {boolean} whether an error should be reported or not
-   */
-  function shouldReport (effectiveParent, name) {
-    return (
-      (!onlyDeclarations || DECLARATION_TYPES.has(effectiveParent.type)) &&
-    !ALLOWED_PARENT_TYPES.has(effectiveParent.type) &&
-    isInvalid(name)
-    );
-  }
-
-  /**
-   * Reports an AST node as a rule violation.
-   *
-   * @param {ASTNode} node The node to report.
-   * @returns {void}
-   * @private
-   */
-  function report (node) {
-    /*
-    * We used the range instead of the node because it's possible
-    * for the same identifier to be represented by two different
-    * nodes, with the most clear example being shorthand properties:
-    * { foo }
-    * In this case, "foo" is represented by one node for the name
-    * and one for the value. The only way to know they are the same
-    * is to look at the range.
-    */
-    if (!reportedNodes.has(node.range.toString())) {
-      const messageId =
-      node.type === 'PrivateIdentifier' ? 'notMatchPrivate' : 'notMatch';
-
-      context.report({
-        data: {
-          name: node.name,
-          pattern,
-        },
-        messageId,
-        node,
-      });
-      reportedNodes.add(node.range.toString());
-    }
-  }
-
-  return {
-    Identifier (node) {
-      const {
-        name,
-      } = node;
-      const {
-        parent,
-      } = node;
-      const effectiveParent =
-        parent.type === 'MemberExpression' ? parent.parent : parent;
-
-      if (parent.type === 'MemberExpression') {
-        if (!checkProperties) {
-          return;
-        }
-
-        // Always check object names
-        if (
-          parent.object.type === 'Identifier' &&
-        parent.object.name === name
-        ) {
-          if (isInvalid(name)) {
-            report(node);
-          }
-
-          // Report AssignmentExpressions left side's assigned variable id
-        } else if (
-          effectiveParent.type === 'AssignmentExpression' &&
-        effectiveParent.left.type === 'MemberExpression' &&
-        effectiveParent.left.property.name === node.name
-        ) {
-          if (isInvalid(name)) {
-            report(node);
-          }
-
-          // Report AssignmentExpressions only if they are the left side of the assignment
-        } else if (
-          effectiveParent.type === 'AssignmentExpression' &&
-        effectiveParent.right.type !== 'MemberExpression' &&
-        isInvalid(name)) {
-          report(node);
-        }
-
-        /*
-        * Properties have their own rules, and
-        * AssignmentPattern nodes can be treated like Properties:
-        * e.g.: const { no_camelcased = false } = bar;
-        */
-      } else if (
-        parent.type === 'Property' ||
-      parent.type === 'AssignmentPattern'
-      ) {
-        if (parent.parent && parent.parent.type === 'ObjectPattern') {
-          if (
-            !ignoreDestructuring &&
-          parent.shorthand &&
-          parent.value.left &&
-          isInvalid(name)
-          ) {
-            report(node);
-          }
-
-          const assignmentKeyEqualsValue =
-          parent.key.name === parent.value.name;
-
-          // Prevent checking righthand side of destructured object
-          if (!assignmentKeyEqualsValue && parent.key === node) {
-            return;
-          }
-
-          const valueIsInvalid = parent.value.name && isInvalid(name);
-
-          // ignore destructuring if the option is set, unless a new identifier is created
-          if (
-            valueIsInvalid &&
-          !(assignmentKeyEqualsValue && ignoreDestructuring)
-          ) {
-            report(node);
-          }
-        }
-
-        // Never check properties or always ignore destructuring
-        if (
-          !checkProperties ||
-        ignoreDestructuring && isInsideObjectPattern(node)
-        ) {
-          return;
-        }
-
-        // Don't check right hand side of AssignmentExpression to prevent duplicate warnings
-        if (parent.right !== node && shouldReport(effectiveParent, name)) {
-          report(node);
-        }
-
-        // Check if it's an import specifier
-      } else if (IMPORT_TYPES.has(parent.type)) {
-        if (ignoreNamedImports && parent.type === 'ImportSpecifier') {
-          // Ignore named import
-        } else if (
-          parent.local &&
-        parent.local.name === node.name &&
-        isInvalid(name)
-        ) {
-          // Report only if the local imported identifier is invalid
-          report(node);
-        }
-      } else if (parent.type === 'PropertyDefinition') {
-        if (checkClassFields && isInvalid(name)) {
-          report(node);
-        }
-
-        // Report anything that is invalid that isn't a CallExpression
-      } else if (shouldReport(effectiveParent, name)) {
-        report(node);
-      }
-    },
-
-    PrivateIdentifier (node) {
-      const isClassField = node.parent.type === 'PropertyDefinition';
-
-      if (isClassField && !checkClassFields) {
-        return;
-      }
-
-      if (isInvalid(node.name)) {
-        report(node);
-      }
-    },
-  };
+const defaultOptions = {
+  classFields: false,
+  ignoreDestructuring: false,
+  ignoreNamedImports: false,
+  onlyDeclarations: false,
+  properties: false,
 };
 
 export default createRule({
-  create,
-  defaultOptions: [],
+  create: (context) => {
+    const [inputPattern, options] = context.options;
+
+    const pattern = inputPattern ?? '^.+$';
+
+    if (typeof pattern !== 'string') {
+      throw new TypeError('Unexpected configuration (a)');
+    }
+
+    const regexp = new RegExp(pattern, 'u');
+
+    if (typeof options !== 'object' && options !== undefined) {
+      throw new TypeError('Unexpected configuration (b)');
+    }
+
+    const checkProperties = Boolean(options?.properties);
+    const checkClassFields = Boolean(options?.classFields);
+    const onlyDeclarations = Boolean(options?.onlyDeclarations);
+    const ignoreDestructuring = Boolean(options?.ignoreDestructuring);
+    const ignoreNamedImports = Boolean(options?.ignoreNamedImports);
+
+    // Contains reported nodes to avoid reporting twice on destructuring with shorthand notation
+    const reportedNodes = new Set();
+
+    const ALLOWED_PARENT_TYPES = new Set(['CallExpression', 'NewExpression']);
+
+    const DECLARATION_TYPES = new Set([
+      'FunctionDeclaration',
+      'VariableDeclarator',
+    ]);
+
+    const IMPORT_TYPES = new Set([
+      'ImportSpecifier',
+      'ImportNamespaceSpecifier',
+      'ImportDefaultSpecifier',
+    ]);
+
+    /**
+     * Checks if a string matches the provided pattern
+     *
+     * @param {string} name The string to check.
+     * @returns {boolean} if the string is a match
+     * @private
+     */
+    const isInvalid = (name) => {
+      return !regexp.test(name);
+    };
+
+    /**
+     * Verifies if we should report an error or not based on the effective
+     * parent node and the identifier name.
+     *
+     * @param {ASTNode} effectiveParent The effective parent node of the node to be reported
+     * @param {string} name The identifier name of the identifier node
+     * @returns {boolean} whether an error should be reported or not
+     */
+    const shouldReport = (effectiveParent, name: string) => {
+      return (
+        (!onlyDeclarations || DECLARATION_TYPES.has(effectiveParent.type)) &&
+        !ALLOWED_PARENT_TYPES.has(effectiveParent.type) &&
+        isInvalid(name)
+      );
+    };
+
+    /**
+     * Reports an AST node as a rule violation.
+     *
+     * @param {ASTNode} node The node to report.
+     * @returns {void}
+     * @private
+     */
+    const report = (node) => {
+      /*
+       * We used the range instead of the node because it's possible
+       * for the same identifier to be represented by two different
+       * nodes, with the most clear example being shorthand properties:
+       * { foo }
+       * In this case, "foo" is represented by one node for the name
+       * and one for the value. The only way to know they are the same
+       * is to look at the range.
+       */
+      if (!reportedNodes.has(node.range.toString())) {
+        const messageId =
+          node.type === AST_NODE_TYPES.PrivateIdentifier
+            ? 'notMatchPrivate'
+            : 'notMatch';
+
+        context.report({
+          data: {
+            name: node.name,
+            pattern,
+          },
+          messageId,
+          node,
+        });
+        reportedNodes.add(node.range.toString());
+      }
+    };
+
+    return {
+      Identifier(node) {
+        const { name } = node;
+        const { parent } = node;
+
+        if (!parent) {
+          return;
+        }
+
+        const effectiveParent =
+          parent.type === AST_NODE_TYPES.MemberExpression
+            ? parent.parent
+            : parent;
+
+        if (!effectiveParent) {
+          return;
+        }
+
+        if (parent.type === AST_NODE_TYPES.MemberExpression) {
+          if (!checkProperties) {
+            return;
+          }
+
+          if (
+            parent.object.type === AST_NODE_TYPES.Identifier &&
+            parent.object.name === name
+          ) {
+            if (isInvalid(name)) {
+              report(node);
+            }
+
+            // Report AssignmentExpressions left side's assigned variable id
+          } else if (
+            effectiveParent.type === AST_NODE_TYPES.AssignmentExpression &&
+            effectiveParent.left.type === AST_NODE_TYPES.MemberExpression &&
+            effectiveParent.left.property.type === AST_NODE_TYPES.Identifier &&
+            effectiveParent.left.property.name === node.name
+          ) {
+            if (isInvalid(name)) {
+              report(node);
+            }
+
+            // Report AssignmentExpressions only if they are the left side of the assignment
+          } else if (
+            effectiveParent.type === AST_NODE_TYPES.AssignmentExpression &&
+            effectiveParent.right.type !== AST_NODE_TYPES.MemberExpression &&
+            isInvalid(name)
+          ) {
+            report(node);
+          }
+
+          /*
+           * Properties have their own rules, and
+           * AssignmentPattern nodes can be treated like Properties:
+           * e.g.: const { no_camelcased = false } = bar;
+           */
+        } else if (
+          parent.type === AST_NODE_TYPES.Property ||
+          parent.type === AST_NODE_TYPES.AssignmentPattern
+        ) {
+          if (parent.parent?.type === AST_NODE_TYPES.ObjectPattern) {
+            if (
+              !ignoreDestructuring &&
+              'shorthand' in parent &&
+              parent.shorthand &&
+              'left' in parent.value &&
+              parent.value.left &&
+              isInvalid(name)
+            ) {
+              report(node);
+            }
+
+            if (!('key' in parent) || !('name' in parent.key)) {
+              throw new Error('OK');
+            }
+
+            const assignmentKeyEqualsValue =
+              'name' in parent.value && parent.key.name === parent.value.name;
+
+            // Prevent checking right-hand side of destructured object
+            if (!assignmentKeyEqualsValue && parent.key === node) {
+              return;
+            }
+
+            const valueIsInvalid =
+              'name' in parent.value && parent.value.name && isInvalid(name);
+
+            // ignore destructuring if the option is set, unless a new identifier is created
+            if (
+              valueIsInvalid &&
+              !(assignmentKeyEqualsValue && ignoreDestructuring)
+            ) {
+              report(node);
+            }
+          }
+
+          // Never check properties or always ignore destructuring
+          if (
+            !checkProperties ||
+            (ignoreDestructuring && isInsideObjectPattern(node))
+          ) {
+            return;
+          }
+
+          // Don't check right hand side of AssignmentExpression to prevent duplicate warnings
+          if (
+            (!('right' in parent) || parent.right !== node) &&
+            shouldReport(effectiveParent, name)
+          ) {
+            report(node);
+          }
+
+          // Check if it's an import specifier
+        } else if (IMPORT_TYPES.has(parent.type)) {
+          if (
+            ignoreNamedImports &&
+            parent.type === AST_NODE_TYPES.ImportSpecifier
+          ) {
+            // Ignore named import
+          } else if (
+            'local' in parent &&
+            parent.local.name === node.name &&
+            isInvalid(name)
+          ) {
+            // Report only if the local imported identifier is invalid
+            report(node);
+          }
+        } else if (parent.type === AST_NODE_TYPES.PropertyDefinition) {
+          if (checkClassFields && isInvalid(name)) {
+            report(node);
+          }
+
+          // Report anything that is invalid that isn't a CallExpression
+        } else if (shouldReport(effectiveParent, name)) {
+          report(node);
+        }
+      },
+    };
+  },
+  defaultOptions: ['^.+$', defaultOptions],
   meta: {
     docs: {
       description:
@@ -266,9 +289,9 @@ export default createRule({
     },
     messages: {
       notMatch:
-        'Identifier \'{{name}}\' does not match the pattern \'{{pattern}}\'.',
+        "Identifier '{{name}}' does not match the pattern '{{pattern}}'.",
       notMatchPrivate:
-        'Identifier \'#{{name}}\' does not match the pattern \'{{pattern}}\'.',
+        "Identifier '#{{name}}' does not match the pattern '{{pattern}}'.",
     },
     schema: [
       {
